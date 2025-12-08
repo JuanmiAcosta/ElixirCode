587683adc40b0fb46233c987bcf123f90657decb
Juanmi Acosta <acostaojuanmi@gmail.com>
2025-12-08 14:02:58 +0100

    My version of the Cards module.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -41,4 +41,73 @@ defmodule Cards do
         card_number <- 1..cards_per_suit(),
         do: {format_card_number(card_number), suit}
   end
+
+  @doc """
+  Shufle a deck. Returns {:ok,deck} if it is a propper deck and {:error,reason} if there is an error
+  """
+  def shuffle(deck) when is_list(deck) do
+    if Enum.any?(deck) && Enum.all?(deck, &is_tuple/1),
+      do: {:ok, Enum.shuffle(deck)},
+      else: {:error, "This is not a keyword list"}
+  end
+
+  def shuffle(_) do
+    {:error, "The deck must be a list"}
+  end
+
+  @doc """
+  Returns if the deck contains this specific card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, {"A", :hearts})
+      true
+  """
+  def contains?(deck, card_to_check) do
+    Enum.member?(deck, card_to_check)
+  end
+
+  @doc """
+  Returns the deal and the remaining deck.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, rest_of_deck} = Cards.deal(deck,1)
+      iex> hand
+      [{"A", :hearts}]
+      iex> Enum.count(rest_of_deck)
+      51
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  @doc """
+  Saves the deck into a file
+  """
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  @doc """
+  Loads a deck from a file
+  """
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file doesn't exist"
+    end
+  end
+
+  @doc """
+  Creates a hand using three functions in a row (create_deck, shuffle and deal)
+  """
+  def create_hand(hand_size) do
+    with {:ok, shuffled_deck} <- create_deck() |> shuffle() do
+      deal(shuffled_deck, hand_size)
+    end
+  end
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,42 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "Create deck returns 52 cards" do
+    deck_length = Enum.count(Cards.create_deck())
+    assert deck_length == 52
+  end
+
+  describe "shuffle/1" do
+    test "returns a shuffled deck as {:ok, deck}" do
+      deck = Cards.create_deck()
+      {:ok, shuffled_deck} = Cards.shuffle(deck)
+
+      assert is_list(shuffled_deck)
+      assert Enum.count(shuffled_deck) == 52
+    end
+
+    test " Shuffling a deck randomizes it" do
+      first_card =
+        Cards.create_deck()
+        |> Cards.shuffle()
+        |> elem(1)
+        |> Enum.at(0)
+
+      assert first_card != {"A", :hearts}
+    end
+
+    test "returns an error if the input is not a list" do
+      invalid_input = "not a deck"
+      result = Cards.shuffle(invalid_input)
+
+      assert result == {:error, "The deck must be a list"}
+    end
+
+    test "returns an error if the deck is not a list of tuples" do
+      invalid_deck = ["A", "B", "C"]
+      result = Cards.shuffle(invalid_deck)
+
+      assert result == {:error, "This is not a keyword list"}
+    end
   end
 end
